526ffcb27ad5aefdcea978f63049eed88df55a86
cidja <chgeorgesc@gmail.com>
2022-05-31 10:46:49 +0200

    ajout des pages view création postController et définition des routes index et contact

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,2 @@
+<li><a href="{{ route('welcome') }}">Accueil</a></li>
+<li><a href="{{ route('contact') }}">Contact</a></li>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +13,5 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index'])->name('welcome');
+Route::get('/contact', [PostController::class, 'contact'])->name('contact');

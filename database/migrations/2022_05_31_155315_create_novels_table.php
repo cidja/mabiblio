d1759a06518c0290f186059ac91e30a7d12d67b3
cidja <chgeorgesc@gmail.com>
2022-06-01 22:08:31 +0200

    add create comment table

--- database/migrations/2022_05_31_155315_create_novels_table.php
+++ database/migrations/2022_05_31_155315_create_novels_table.php
@@ -15,7 +15,18 @@ return new class extends Migration
     {
         Schema::create('novels', function (Blueprint $table) {
             $table->id();
-            
+            $table->string('title');
+            $table->string('author_firstname');
+            $table->string('author_lastname');
+            $table->bigInteger('isbn');
+            $table->string('book_type');
+            $table->integer('pages_nb');
+            $table->integer('volume_nb');
+            $table->boolean('active');
+            $table->boolean('finish');
+            //$table->string('cover') mettre dans une migrations novel_image
+            $table->dateTime('begin_at');
+            $table->dateTime('end_at');
             $table->timestamps();
         });
     }
---
+++ database/migrations/2022_06_01_193629_create_comments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('content');
+            $table->integer('rate');
+            $table->unsignedBigInteger('novels_id'); //pour la relation entre novel_id et comment
+            $table->foreign('novels_id')->references('id')->on('novels');
+
+            $table->unsignedBigInteger('cartoons_id'); // pour la relation entre carton_id et comment
+            $table->foreign('cartons_id')->references('id')->on('cartoons');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};

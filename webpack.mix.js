9d84e46a9b8ed02785ffb30a139e198251491e23
root <root@localhost.localdomain>
2022-08-03 15:13:20 +0000

    ajout des fichiers sur ionos

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,20 +6,11 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-/* J'ai viré tailwind pour repasser sous bootstrap source: https://larabiz.fr/blog/comment-bootstrap-5-laravel 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        require('tailwindcss'),
-    ]);
-    */
-    
-    mix
-    .css('resources/css/app.css', 'public/css')
-    .js('resources/js/app.js', 'public/js')
-    .version()
-    
+    .react()
+    .sass('resources/sass/app.scss', 'public/css');
